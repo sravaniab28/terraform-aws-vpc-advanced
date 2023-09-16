4fa3c089cebd3cad682df5110c17f46bd6e20783
sravani <sravaniab28@gmail.com>
2023-09-16 17:30:19 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,96 @@
+variable "cidr_block" {
+  
+}
+variable "enable_dns_hostnames" {
+    default = true
+  
+}
+
+variable "enable_dns_support" {
+    default = true
+  
+}
+variable "project_name" {
+  
+}
+variable "common_tags" {
+  default = {}
+}
+
+variable "vpc_tags" {
+    default = {}
+  
+}
+variable "igw_tags" {
+    default = {}
+  
+}
+
+variable "public_subnet_cidr" {
+    type = list
+   validation {
+   
+    condition = length(var.public_subnet_cidr) == 2
+     error_message = "please provide 2 public subnet cidr"
+  }
+}
+
+variable "private_subnet_cidr" {
+    type = list
+   validation {
+   
+    condition = length(var.private_subnet_cidr) == 2
+     error_message = "please provide 2 private subnet cidr"
+  }
+}
+
+variable "database_subnet_cidr" {
+    type = list
+   validation {
+   
+    condition = length(var.database_subnet_cidr) == 2
+     error_message = "please provide 2 database subnet cidr"
+  }
+}
+variable "nat_gateway_tags" {
+  
+  default = {}
+}
+
+variable "public_route_table_tags" {
+  default = {}
+}
+
+
+variable "private_route_table_tags" {
+  default = {}
+}
+
+
+variable "database_route_table_tags" {
+  default = {}
+}
+
+variable "db_subnet_group_tags" {
+  default = {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
